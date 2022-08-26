5f4ef9f26f064981bdb98fef45cc3cbb61877d30
leon0399 <hello@leon0399.ru>
2022-08-26 15:18:48 +0300

    feat: update logger

--- sweep.js
+++ sweep.js
@@ -3,9 +3,9 @@
 process.on('warning', e => console.warn(e.stack));
 
 import { program, InvalidArgumentError } from 'commander'
-import _progress, { SingleBar, MultiBar } from 'cli-progress'
+import ProgressBar from 'progress'
 
-import ethers from 'ethers'
+import ethers, { BigNumber } from 'ethers'
 import { getNetwork } from '@ethersproject/networks'
 import { getAddress } from '@ethersproject/address'
 import { HDNode, isValidMnemonic } from '@ethersproject/hdnode'
@@ -37,7 +37,7 @@ program
         return mnemonic
     })
     .option('-a, --to-address <address>', 'send all funds to this address', getAddress)
-    .option('-G, --gas <gas>',            'gas price')
+    .option('-G, --gas <gas>',            'Gas price in GWEI')
     .option('-n, --network <network>',    'Ethereum network to use', getNetwork, 1)
 
 program.parse()
@@ -45,12 +45,17 @@ program.parse()
 const opts = program.opts()
 
 const hdNode = HDNode.fromMnemonic(opts.mnemonic)
+console.log('🔑  Read mnemonic successfully')
+console.log('⏳  Connecting to providers')
 const provider = ethers.getDefaultProvider(opts.network, {
     infura: opts.infura,
     etherscan: opts.etherscan
 })
 
 const etherscan = new ethers.providers.EtherscanProvider(opts.network, opts.etherscan)
+console.log('🌍  Connected to all providers successfully')
+
+const gasPriceWei = ethers.utils.parseUnits(opts.gas, 'gwei')
 
 /** @param {string} address */
 async function getEthBalance(address) {
@@ -75,71 +80,58 @@ function formatPath(index = '1', change = '1', account = '1', coinType = '60', p
     return `m/${purpose}'/${coinType}'/${account}'/${change}/${index}`
 }
 
-const totalChanges = 5; // to check/sweep
-const totalIndexes = 5; // to check/sweep
+const totalIndexes = 250; // to check/sweep
 
+const purposes = [44, 49, 84, 0]
 const coinTypes = [60, 61, ]
-const accounts = [0, 1, 160720]
-const changes = range(totalChanges)
+const accounts = [0, 1, 160720, 2147483644, 2147483645, 2147483646, 2147483647]
+const changes = [0, 1]
 const indexes = range(totalIndexes)
 
 async function sweep() {
-    const progress = new MultiBar({
-        format: '{bar} | {path} | ETA: {eta}s | {value}/{total}',
-        hideCursor: true,
-    }, _progress.Presets.shades_grey)
+    const total = coinTypes.length * accounts.length * changes.length * indexes.length;
 
-    const coinsBar = progress.create(coinTypes.length, 0);
-    const accountsBar = progress.create(accounts.length, 0);
-    const changesBar = progress.create(changes.length, 0);
-    const indexesBar = progress.create(indexes.length, 0);
+    const bar = new ProgressBar(':bar (:current/:total) :path', { 
+        total,
+        width: 120
+    })
 
-    coinsBar.start(coinTypes.length, 0)
-    for (let coinType of coinTypes) {
-        coinsBar.update({ path: formatPath('*', '*', '*', coinType) })
+    for (let purposeIndex of purposes) {
+        for (let coinType of coinTypes) {
+            for (let accountIndex of accounts) {
+                for (let changeIndex of changes) {
+                    for (let addressIndex of indexes) {
+                        const path = formatPath(addressIndex, changeIndex, accountIndex, coinType, purposeIndex)
 
-        accountsBar.start(accounts.length, 0)
-        for (let a of accounts) {
-            accountsBar.update({ path: formatPath('*', '*', a, coinType) })
+                        bar.tick({ path })
 
-            changesBar.start(changes.length, 0)
-            for (let c of changes) {
-                changesBar.update({ path: formatPath('*', c, a, coinType) })
+                        const currentWallet = hdNode.derivePath(path)
+                        const address = currentWallet.address
 
-                indexesBar.start(indexes.length, 0)
-                for (let i of indexes) {
-                    const path = formatPath(i, c, a, coinType)
+                        await checkBalance(currentWallet, console.log)
 
-                    const currentWallet = hdNode.derivePath(path)
-                    const address = currentWallet.address
-
-                    indexesBar.update({ path })
-
-                    checkBalance(address, progress.log)
-
-                    indexesBar.increment()
+                        // bar.tick()
+                    }
                 }
-
-                changesBar.increment()
             }
-
-            accountsBar.increment()
         }
-
-        coinsBar.increment()
     }
 }
 
 /**
  *
- * @param {string} address
+ * @param {HDNode} wallet
  */
-async function checkBalance(address, log) {
+async function checkBalance(wallet, log) {
+    const address = wallet.address
+    const path = wallet.path
+
     const [
         balance,
         erc20Txs,
         erc721Txs,
         erc1155Txs,
+        nonce
     ] = await Promise.all([
         getEthBalance(address),
         etherscan.fetch('account', {
@@ -153,32 +145,67 @@ async function checkBalance(address, log) {
         etherscan.fetch('account', {
             action: "token1155tx",
             address: address,
-        })
+        }),
+        provider.getTransactionCount(address),
     ])
 
-    if (balance.gt(0)) {
-        log('Found non-null ETH account: Path: ' + path + '\t Address: ' + address + '\t ETH: ' + ethers.utils.formatEther(balance) + '\n')
+    const [
+        hasBalance,
+        hasErc20,
+        hasErc721,
+        hasErc1155,
+        hasTxs,
+    ] = [
+        balance.gt(0),
+        erc20Txs && erc20Txs.length > 0,
+        erc721Txs && erc721Txs.length > 0,
+        erc1155Txs && erc1155Txs.length > 0,
+        nonce > 0,
+    ]
 
-        // TODO: send ETH to address
+    if (hasBalance || hasErc20 || hasErc721 || hasErc1155 || hasTxs) {
+        log(`\n🕵️‍♂️  Found acive account at path=${path}, address=${address} => `)
+
+        if (hasBalance) {
+            log(`\tETH: ${ethers.utils.formatEther(balance)}`)
+
+            // TODO: send ETH to address
+        }
+
+        if (hasErc20) {
+            log(`\tERC20`)
+
+            // TODO: send ERC20 to address
+        }
+
+        if (hasErc721) {
+            log(`\tERC721`)
+
+            // TODO: send NFT to address
+        }
+
+        if (hasErc1155) {
+            log(`\tERC1155`)
+
+            // TODO: send NFT to address
+        }
+
+        if (hasTxs) {
+            log(`\tNonce: ${nonce}`)
+        }
     }
+}
 
-    if (erc20Txs && erc20Txs.length > 0) {
-        log('Found non-null ERC20 account: Path: ' + path + '\t Address: ' + address + '\n')
-
-        // TODO: send ERC20 to address
-    }
-
-    if (erc721Txs && erc721Txs.length > 0) {
-        log('Found non-null ERC721 account: Path: ' + path + '\t Address: ' + address + '\n')
-
-        // TODO: send NFT to address
-    }
-
-    if (erc1155Txs && erc1155Txs.length > 0) {
-        log('Found non-null ERC1155 account: Path: ' + path + '\t Address: ' + address + '\n')
-
-        // TODO: send NFT to address
-    }
+/**
+ *
+ * @param {ethers.utils.HDNode} wallet
+ * @param {BigNumberish} balance
+ */
+function sendEthAssets(wallet, balance) {
+    const gasLimit = BigNumber.from('21000')
+    const maxCostWei = gasPriceWei.mul(gasLimit)
+    const realBalance = BigNumber.from(balance)
+    const value = realBalance.sub(maxCostWei)
 }
 
 sweep()
